a69767aed4801125bbd67c7525a93db17f3d476b
venkatchandru <venkatchandru88@gmail.com>
2021-11-07 16:22:37 +0530

    first commit

---
+++ src/org/add/GreenTech.java
@@ -0,0 +1,23 @@
+package org.add;
+
+public class GreenTech {
+	
+	private void greensadyar() {
+    System.out.println("Adyar address is : LB road");
+	}
+	private void greeensOmr() {
+System.out.println("Omr address is : Thuraipakkam");
+	}
+private void greensVelachery() {
+System.out.println("Velachery address is :Vijaya nagar");
+}
+	public static void main(String[] args) {
+		GreenTech a = new GreenTech();
+		a.greeensOmr();
+		a.greensadyar();
+		a.greensVelachery();
+				
+	}
+	
+	
+}
